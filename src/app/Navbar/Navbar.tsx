5bb68cb59569fa1a2c0d9bbb729736fa3fb0230c
lorenzo <lorenzo@ethzero.it>
2025-05-23 13:59:15 +0200

    Add files via upload

---
+++ src/app/Navbar/Navbar.tsx
@@ -0,0 +1,54 @@
+import logo from "@/assets/logo.png";
+import Link from "next/link";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import { getCart } from "@/lib/db/cart";
+import ShoppingCartButton from "./ShoppingCartButton";
+import UserMenuButton from "./UserMenuButton";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+
+async function searchProducts(formData: FormData) {
+  "use server";
+
+  const searchQuery = formData.get("searchQuery")?.toString();
+  if (searchQuery) {
+    redirect("/search?query=" + searchQuery);
+  }
+}
+
+export default async function Navbar() {
+  const session = await getServerSession(authOptions);
+  const cart = await getCart();
+  return (
+    <div className="bg-base-100">
+      <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
+        <div className="flex-1">
+          <Link href="/" className="text-xl normal-case">
+            <Image
+              unoptimized
+              src={logo}
+              height={100}
+              width={150}
+              alt="Gucci - Platform Engineering"
+            />
+          </Link>
+          Platform Engineering - Demo Application
+        </div>
+        <div className="flex-none gap-2">
+          <form action={searchProducts}>
+            <div className="form-control">
+              <input
+                name="searchQuery"
+                placeholder="Search"
+                className="input input-bordered w-full min-w-[100px]"
+              />
+            </div>
+          </form>
+          <ShoppingCartButton cart={cart} />
+          <UserMenuButton session={session} />
+        </div>
+      </div>
+    </div>
+  );
+}
